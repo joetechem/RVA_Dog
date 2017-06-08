282f543571a6f02f1458b2ae6709e844ad7f5d53
joe <joe@techemstudios.com>
2017-06-08 15:21:56 -0400

    main template

---
+++ src/main.py
@@ -0,0 +1,241 @@
+"""
+Simple Python Lambda function providing helpful tips for new parents with a newborn.
+Intents supported:
+    Open
+    GetHelp
+    Stop
+    Gas
+    Cycle
+    Bored
+    NothingWorks
+"""
+
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+# --------------- Helpers that build all of the responses ----------------------
+
+def build_speechlet_response(card_title, card_content, output, reprompt_text, should_end_session):
+    return {
+        'outputSpeech': {
+            'type': 'SSML',
+            'ssml': output
+        },
+        'card': {
+            'type': 'Simple',
+            'title': card_title,
+            'content': card_content
+        },
+        'reprompt': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': reprompt_text
+            }
+        },
+        'shouldEndSession': should_end_session
+    }
+
+
+def build_response(session_attributes, speechlet_response):
+    return {
+        'version': '1.0',
+        'sessionAttributes': session_attributes,
+        'response': speechlet_response
+    }
+
+
+# --------------- Your functions to implement your intents ------------------
+
+def dog_parks(intent, session):
+    session_attributes = {}
+    reprompt_text = None
+    speech_output = ""
+    should_end_session = True
+
+    card_output = "  "
+    speech_output = "<speak>Here are several dog parks in Richmond, Virginia... Barker Field, Northside Dog Park, Church Hill Dog Park</speak>"
+
+    return build_response(session_attributes, build_speechlet_response
+                          ("Dog Parks", card_output, speech_output, reprompt_text, should_end_session))
+
+
+def function2(intent, session):
+    session_attributes = {}
+    reprompt_text = None
+    speech_output = ""
+    should_end_session = True
+
+    card_output = ""
+    speech_output = "<speak></speak>"
+
+    return build_response(session_attributes, build_speechlet_response
+                          ("", card_output, speech_output, reprompt_text, should_end_session))
+
+
+def function3(intent, session):
+    session_attributes = {}
+    reprompt_text = None
+    speech_output = ""
+    should_end_session = True
+    
+    card_output = ""
+    speech_output = "<speak></speak>"
+
+    return build_response(session_attributes, build_speechlet_response
+                          ("", card_output, speech_output, reprompt_text, should_end_session))
+
+
+def function4(intent, session):
+    session_attributes = {}
+    reprompt_text = None
+    speech_output = ""
+    should_end_session = True
+    
+    card_output = ""
+    speech_output = "<speak></speak>"
+
+    return build_response(session_attributes, build_speechlet_response
+                          ("", card_output, speech_output, reprompt_text, should_end_session))
+
+
+def stop(intent, session):
+    session_attributes = {}
+    reprompt_text = None
+    speech_output = ""
+    should_end_session = True
+    
+    card_output = ""
+    speech_output = "<speak></speak>"
+
+    return build_response(session_attributes, build_speechlet_response
+                          ("Session Ended", card_output, speech_output, reprompt_text, should_end_session))
+                          
+
+def open_it(intent, session):
+    session_attributes = {}
+    reprompt_text = None
+    speech_output = ""
+    should_end_session = False
+    
+    card_output = "Welcome to"
+    speech_output = "<speak></speak>"
+
+    return build_response(session_attributes, build_speechlet_response
+                          ("RVA Dog", card_output, speech_output, reprompt_text, should_end_session))
+
+
+def handle_session_end_request():
+    card_title = "Session Ended"
+    speech_output = "Thank you for asking RVA Dog."
+    should_end_session = True
+    return build_response({}, build_speechlet_response(
+        card_title, speech_output, None, should_end_session))
+
+# --------------- Primary Events ------------------
+
+def on_session_started(session_started_request, session):
+    """ Called when the session starts """
+
+    logger.info("on_session_started requestId=" + session_started_request['requestId'] +
+                ", sessionId=" + session['sessionId'])
+                
+
+def on_launch(launch_request, session):
+    """ Called when the user launches the skill without specifying what they
+    want
+    """
+
+    logger.info("on_launch requestId=" + launch_request['requestId'] +
+                ", sessionId=" + session['sessionId'])
+    
+    # Dispatch to skill's launch
+    return build_response({},build_speechlet_response(
+        "RVA Dog", "Welcome to the RVA Dog skill.</speak>","",False))
+
+
+def get_help(intent, session):
+    """ Called when the user asks for help """
+    session_attributes = {}
+    reprompt_text = None
+    speech_output = ""
+    should_end_session = False
+    
+    card_output = "Here are some things you can ask or tell RVA Dog:"
+    speech_output = "<speak>Need help navigating RVA Dog? Here are some keywords RVA Dog understands.</speak>"
+
+    return build_response(session_attributes, build_speechlet_response
+                          ("How to Ask JoeDaddy", card_output, speech_output, reprompt_text, should_end_session))
+
+
+def on_intent(intent_request, session):
+    """ Called when the user specifies an intent for this skill """
+
+    logger.info("on_intent requestId=" + intent_request['requestId'] +
+                ", sessionId=" + session['sessionId'])
+
+    intent = intent_request['intent']
+    intent_name = intent_request['intent']['name']
+
+    # Dispatch to skill's intent handlers
+
+    if intent_name == "DogPark":
+        return nothing_works(intent, session)
+    elif intent_name == "Function2":
+        return gas(intent, session)
+    elif intent_name == "Function3":
+        return cycle(intent, session)
+    elif intent_name == "Function4":
+        return bored(intent, session)
+    elif intent_name == "Stop":
+        return stop(intent, session)
+    elif intent_name == "Open":
+        return open_it(intent, session)
+    elif intent_name == "GetHelp":
+        return get_help(intent, session)
+#    elif intent_name == "AMAZON.HelpIntent":
+#        return get_help()
+    elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
+        return handle_session_end_request()
+    else:
+        raise ValueError("Invalid intent")
+
+def on_session_ended(session_ended_request, session):
+    """ Called when the user ends the session.
+    Is not called when the skill returns should_end_session=true
+    """
+    print("on_session_ended requestId=" + session_ended_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # add cleanup logic here
+
+
+# --------------- Main handler ------------------
+
+def lambda_handler(event, context):
+    """ Route the incoming request based on type (LaunchRequest, IntentRequest,
+    etc.) The JSON body of the request is provided in the event parameter.
+    """
+    logger.info("event.session.application.applicationId=" +
+          event['session']['application']['applicationId'])
+
+    """
+    Uncomment this if statement and populate with skill's application ID to
+    prevent someone else from configuring a skill that sends requests to this
+    function.
+    """
+    # if (event['session']['application']['applicationId'] !=
+    #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
+    #     raise ValueError("Invalid Application ID")
+
+    if event['session']['new']:
+        on_session_started({'requestId': event['request']['requestId']},
+                           event['session'])
+
+    if event['request']['type'] == "LaunchRequest":
+        return on_launch(event['request'], event['session'])
+    elif event['request']['type'] == "IntentRequest":
+        return on_intent(event['request'], event['session'])
+    else:
+        return on_session_ended(event['request'], event['session'])
